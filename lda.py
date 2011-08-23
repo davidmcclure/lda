d0a5d89e8a1bee821d7e005b26f1a3154908e249
David McClure <david.mcclure@virginia.edu>
2011-08-23 09:56:56 -0400

    gibs implemented, not working well

--- lda.py
+++ lda.py
@@ -1,4 +1,7 @@
 import re
+import numpy as np
+
+np.set_printoptions(threshold='nan')
 
 class Document(object):
 
@@ -17,21 +20,23 @@ class Document(object):
     # pushed onto the core words list.
     WORD_REGEX = "^[a-z']+$"
 
+
     def __init__(self, filepath):
         '''
-        Set source file location, build contractions list, and
-        initialize empty lists for lines and words.
+        Set source file location, build contractions list, and initialize empty
+        lists for lines and words.
         '''
         self.filepath = filepath
         self.file = open(self.filepath)
         self.lines = []
         self.words = []
 
+
     def split(self):
         '''
-        Split file into an ordered list of words. Scrub out
-        punctuation; lowercase everything; preserve contractions;
-        disallow strings that include non-letters.
+        Split file into an ordered list of words. Scrub out punctuation;
+        lowercase everything; preserve contractions; disallow strings that
+        include non-letters.
         '''
         self.lines = [line for line in self.file]
         for line in self.lines:
@@ -41,12 +46,13 @@ class Document(object):
                 if clean_word:
                     self.words.append(clean_word)
 
+
     def _clean_word(self, word):
         '''
-        Parses a space-delimited string from the text and determines
-        whether or not it is a valid word. Scrubs punctuation, retains
-        contraction apostrophes. If cleaned word passes final regex,
-        returns the word; otherwise, returns None.
+        Parses a space-delimited string from the text and determines whether or
+        not it is a valid word. Scrubs punctuation, retains contraction
+        apostrophes. If cleaned word passes final regex, returns the word;
+        otherwise, returns None.
         '''
         word = word.lower()
         for punc in Document.PUNCTUATION + Document.CARRIAGE_RETURNS:
@@ -66,8 +72,79 @@ class Corpus(object):
         '''
         self.documents = []
 
+
     def add_document(self, document):
         '''
-        Add a document (Document instantiation) to the corpus.
+        Add a document to the corpus.
         '''
         self.documents.append(document)
+
+
+    def build_vocabulary(self):
+        '''
+        Construct a list of unique words in the corpus.
+        '''
+        # ** ADD ** #
+        # exclude words that appear in 90%+ of the documents
+        # exclude words that are too (in)frequent
+        discrete_set = set()
+        for document in self.documents:
+            for word in document.words:
+                discrete_set.add(word)
+        self.vocabulary = list(discrete_set)
+
+
+    def lda(self, number_of_topics, iterations, alpha, beta):
+
+        '''
+        Model topics.
+        '''
+
+        # Get vocabulary and number of documents.
+        self.build_vocabulary()
+        number_of_documents = len(self.documents)
+        vocabulary_size = len(self.vocabulary)
+
+        # Create the counter arrays.
+        self.document_topic_counts = np.zeros([number_of_documents, number_of_topics], dtype=np.int)
+        self.topic_word_counts = np.zeros([number_of_topics, len(self.vocabulary)], dtype=np.int)
+        self.current_word_topic_assignments = []
+        self.topic_counts = np.zeros(number_of_topics)
+
+        # Initialize
+        for d_index, document in enumerate(self.documents):
+            word_topic_assignments = []
+            for word in document.words:
+                if word in self.vocabulary:
+                    # Select random starting topic assignment for word.
+                    w_index = self.vocabulary.index(word)
+                    starting_topic_index = np.random.randint(number_of_topics)
+                    word_topic_assignments.append(starting_topic_index)
+                    # Set current topic assignment, increment doc-topic and word-topic counters.
+                    self.document_topic_counts[d_index, starting_topic_index] += 1
+                    self.topic_word_counts[starting_topic_index, w_index] += 1
+                    self.topic_counts[starting_topic_index] += 1
+            self.current_word_topic_assignments.append(np.array(word_topic_assignments))
+
+        # Run the sampler.
+        for iteration in range(iterations):
+            for d_index, document in enumerate(self.documents):
+                for w, word in enumerate(document.words):
+                    if word in self.vocabulary:
+                        w_index = self.vocabulary.index(word)
+                        # Get the topic that the word is currently assigned to.
+                        current_topic_index = self.current_word_topic_assignments[d_index][w]
+                        # Decrement counts.
+                        self.document_topic_counts[d_index, current_topic_index] -= 1
+                        self.topic_word_counts[current_topic_index, w_index] -= 1
+                        self.topic_counts[current_topic_index] -= 1
+                        # Get new topic.
+                        topic_distribution = (self.topic_word_counts[:, w_index] + beta) * \
+                            (self.document_topic_counts[d_index] + alpha) / \
+                            (self.topic_counts[current_topic_index] + beta)
+                        new_topic_index = np.random.multinomial(1, np.random.dirichlet(topic_distribution)).argmax()
+                        # Reassign and notch up counts.
+                        self.current_word_topic_assignments[d_index][w] = new_topic_index
+                        self.document_topic_counts[d_index, new_topic_index] += 1
+                        self.topic_word_counts[new_topic_index, w_index] += 1
+                        self.topic_counts[new_topic_index] += 1
--- usage/dev.py
+++ usage/dev.py
@@ -7,9 +7,15 @@ import lda
 corpus = lda.Corpus() # instantiate corpus
 
 # iterate over the files in the directory.
-document_path = '../texts/grimm_fairy_tales'
-for document_file in glob.glob(os.path.join(document_path, '*.txt')):
-    document = lda.Document(document_file) # instantiate document
-    document.split() # tokenize
-    document.build_document_wordcount_matrix() # populate word:wordcount matrix
-    corpus.add_document(document) # push onto corpus documents list
+document_paths = ['../texts/grimm_fairy_tales', '../texts/tech_blog_posts', '../texts/nyt']
+for document_path in document_paths:
+    for document_file in glob.glob(os.path.join(document_path, '*.txt')):
+        document = lda.Document(document_file) # instantiate document
+        document.split() # tokenize
+        corpus.add_document(document) # push onto corpus documents list
+
+corpus.build_vocabulary()
+print len(corpus.vocabulary)
+# corpus.lda(100, 30, 2.5, 0.1)
+# print corpus.topic_word_counts
+# print corpus.document_topic_counts
